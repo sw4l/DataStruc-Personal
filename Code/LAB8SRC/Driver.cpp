1b1b65d75b2cf7d1d385a525b08c476b724d7ad9
sw4l <shhs11@live.com>
2016-03-14 23:40:36 -0500

    LAB8 SOURCE FILES

---
+++ Code/LAB8SRC/Driver.cpp
@@ -0,0 +1,23 @@
+//============================================================================
+// Name        : Driver.cpp
+// Author      : 
+// Version     :
+// Copyright   :
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+#include "Stack.h"
+#include "SuperStack.h"
+int findMin(int array[], int length);
+int main() {
+
+}
+//our find minimum function of run time O(LOG(N))
+int findMin(int array[], int length) {
+	//binary search
+
+	return 0;
+}
---
+++ Code/LAB8SRC/SuperStack.h
@@ -0,0 +1,87 @@
+/*
+ * SuperStack.h
+ *
+ *  Created on: Mar 10, 2016
+ *      Author: Dakota
+ */
+
+#ifndef SUPERSTACK_H_
+#define SUPERSTACK_H_
+#include <iostream>
+#include "ArrayStack.h"
+template<class T>
+class SuperStack {
+	ArrayStack<T> maxValue;
+	ArrayStack<T> myStack;
+public:
+	SuperStack();
+	virtual ~SuperStack();
+	bool push(T* element);
+	T* pop();
+	T* peek();
+	T* getMax();
+
+};
+//constructor
+template<class T>
+SuperStack<T>::SuperStack() {
+
+}
+//destructor
+template<class T>
+SuperStack<T>::~SuperStack() {
+
+}
+//our method implementations
+
+/*
+ * we check if the element is null if not push
+ * update maxValue if element is greater than current MaxValue
+ */
+template<class T>
+bool SuperStack<T>::push(T* element) {
+	if (element != NULL) {
+		myStack.push(element);
+		//if the added element is larger than our stored max value, push it to our maxValue stack
+		if (*element > *maxValue.peek()) {
+			maxValue.push(element);
+		}
+		return true;
+	} else {
+		return false;
+	}
+}
+
+//we just call stack pop and return the removed element
+template<class T>
+T* SuperStack<T>::pop() {
+	T* temp;
+	if (!myStack.isEmpty()) {
+		temp = myStack.peek();
+		myStack.pop();
+		//checking if our maxValue thats stored on the tracked stack is being deleted if so pop it out of maxValue
+		if (*temp == *maxValue.peek()) {
+			maxValue.pop();
+		}
+		return temp;
+	} else {
+		return NULL;
+	}
+}
+
+template<class T>
+T* SuperStack<T>::peek() {
+	T* temp;
+	if (!myStack.isEmpty()) {
+		temp = myStack.peek();
+		return temp;
+	} else {
+		return NULL;
+	}
+}
+
+template<class T>
+T* SuperStack<T>::getMax() {
+	return this->maxValue;
+}
+#endif /* SUPERSTACK_H_ */
